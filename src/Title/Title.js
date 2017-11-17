139507567f67fb33a7c1997d4701158ed75df48d
Travis Rollins <kalikoze@gmail.com>
2017-11-16 18:43:47 -0700

    Add video and create oreate overlay with title.

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Power from './assets/03 - Power.mp3'
+import Power from './assets/03 - Power.mp3';
+import codeLoop from './assets/Code-Loop.mov';
 import './Title.css';
 
 const Title = () => {
   return (
     <section className="title">
       <audio src={Power} autoPlay loop />
-
+      <video src={codeLoop} autoPlay loop />
+      <article className="overlay">
+        <h1>Developing For The Future</h1>
+      </article>
     </section>
   )
 }
