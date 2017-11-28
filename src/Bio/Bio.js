d96caf498209f6b8056c70cec32aa09134fe76bd
Travis Rollins <kalikoze@gmail.com>
2017-11-27 18:21:42 -0700

    Refactor and clean up css.

--- src/Bio/Bio.js
+++ src/Bio/Bio.js
@@ -15,9 +15,8 @@ export default class Bio extends Component {
           <article className="bio-text">
             <p>
               As a Software Developer, Travis strives to push himself to the limits as well as help motivate his colleagues to be the best they can. Although he has been a professional musician for more than 10 years, he is always trying to learn something new. He thrives off of new challenges and learning new technologies as well as working with people to create new experiences. He hopes to continue working in an environment that is built on teamwork and strives to challenge the norm.
-            </p>
-            <br />
-            <p>
+              <br />
+              <br />
               After recently finishing the Front-End program at the Turing School of Software & Design, he has learned the importance of UI/UX experience, testing on multiple levels, and the power of diversity.  His strengths lie in creating simple and clean interfaces, bringing in various types of data, developing accounts with authorization, and pushing unique experiences.   With one ear to the sound of a new drum, and the other to the wisdom of other developers willing to impart their advice, Travis continues towards a new, fulfilling, and successful career.
             </p>
           </article>
