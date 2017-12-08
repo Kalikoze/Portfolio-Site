3c20efcb264967e265bf2bc9264a38566d57a178
Travis Rollins <kalikoze@gmail.com>
2017-12-07 22:27:57 -0700

    Add functionality to mobile header.

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import menu from './assets/menu.svg';
+import React, { Component } from 'react';
 import './Header.css';
 
-const Header = () => {
-  return (
-    <nav>
-      <section className="mobile-menu">
-        {/* <img src={menu} alt="Menu icon" /> */}
-        <h2 className="menu-text">Menu</h2>
-      </section>
-      <ul>
-        <a href='#home'>
-          <p className="nav-text">Home</p>
-        </a>
-        <a href='#bio'>
-          <p className="nav-text">Bio</p>
-        </a>
-        <a href='#tech'>
-          <p className="nav-text">Tech</p>
-        </a>
-        <a href='#projects'>
-          <p className="nav-text">Projects</p>
-        </a>
-        <a href='#contact'>
-          <p className="nav-text">Contact</p>
-        </a>
-      </ul>
-    </nav>
-  )
-}
+export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dropDown: false
+    }
+  }
 
-export default Header;
+  render() {
+    const { dropDown } = this.state;
+
+    return (
+      <nav onClick={() => this.setState({dropDown: !dropDown})}>
+        <section className="mobile-menu">
+          <h2 className="menu-text">Menu</h2>
+        </section>
+        <ul className={ dropDown ? 'drop-down' : ''}>
+          <a href='#home' className={ dropDown ? 'drop-link' : ''}>
+            <p className="nav-text">Home</p>
+          </a>
+          <a href='#bio' className={ dropDown ? 'drop-link' : ''}>
+            <p className="nav-text">Bio</p>
+          </a>
+          <a href='#tech' className={ dropDown ? 'drop-link' : ''}>
+            <p className="nav-text">Tech</p>
+          </a>
+          <a href='#projects' className={ dropDown ? 'drop-link' : ''}>
+            <p className="nav-text">Projects</p>
+          </a>
+          <a href='#contact' className={ dropDown ? 'drop-link' : ''}>
+            <p className="nav-text">Contact</p>
+          </a>
+        </ul>
+      </nav>
+    )
+  }
+}
